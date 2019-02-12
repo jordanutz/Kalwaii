6ff0a7c3b0a7852ab4a28e16ccbf1b31b7044bb0
jordanutz <jordanutz@outlook.com>
2019-02-12 12:44:26 -0700

    Initial commit

---
+++ db/init.sql
@@ -0,0 +1,58 @@
+drop table food
+drop table meal
+drop table diet_users
+drop table diet_profile
+
+select * from diet_users
+select * from diet_profile
+select * from meal
+select * from food
+select * from user_records
+
+create table diet_users (
+    id serial primary key,
+    auth0_id text,
+    username text,
+    email text,
+    photo text
+)
+
+create table diet_profile (
+    id serial primary key,
+    user_id integer references diet_users(id),
+    gender integer not null,
+    age integer not null,
+    height integer not null,
+    weight integer not null,
+    physical_level int not null,
+    body_fat int not null
+)
+
+create table user_records (
+    id serial primary key,
+    user_id integer references diet_users(id),
+    meal_id integer references meal(id),
+    food_id integer references food(id),
+    date_posted date
+)
+
+create table food (
+    id serial primary key,
+    name varchar(40) not null,
+    calories decimal not null,
+    preparation text not null,
+    protein decimal not null,
+    carbohydrates decimal not null,
+    fat decimal not null,
+    sugar decimal not null, 
+    saturated_fat decimal not null,
+    unsaturated_fat decimal not null,
+    cholesterol decimal not null,
+    sodium decimal not null,
+    potassium decimal not null
+)
+
+create table meal (
+    id serial primary key,
+    meal varchar(40) not null
+)
